4ce53166e358c1beb5af2e0a821f53c991046a0b
snowolf001 <tyindl@gmail.com>
2026-09-23 15:21:42 -0600

    Expose Health Connect history permission bridge

--- specs/NativeHealthPermissions.ts
+++ specs/NativeHealthPermissions.ts
@@ -2,5 +2,7 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 export interface Spec extends TurboModule {
   requestWritePermission(recordType: string): Promise<boolean>;
+  isHistoryReadAvailable(): Promise<boolean>;
+  requestHistoryReadPermission(): Promise<boolean>;
 }
 export default TurboModuleRegistry.get<Spec>('HealthPermissions');
