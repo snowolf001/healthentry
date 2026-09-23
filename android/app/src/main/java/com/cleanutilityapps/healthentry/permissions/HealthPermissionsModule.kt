be3b8085c1d5198ba0d4767b59cef29cc4476265
snowolf001 <tyindl@gmail.com>
2026-09-23 15:21:54 -0600

    Bridge Health Connect history feature and permission

--- android/app/src/main/java/com/cleanutilityapps/healthentry/permissions/HealthPermissionsModule.kt
+++ android/app/src/main/java/com/cleanutilityapps/healthentry/permissions/HealthPermissionsModule.kt
@@ -14,5 +14,21 @@ class HealthPermissionsModule(context: ReactApplicationContext) : NativeHealthPe
             else owner.healthPermissions.request(recordType, promise)
         }
     }
+    override fun isHistoryReadAvailable(promise: Promise) {
+        UiThreadUtil.runOnUiThread {
+            val owner = reactApplicationContext.currentActivity as? HealthPermissionOwner
+            if (owner == null) promise.resolve(false)
+            else promise.resolve(owner.healthPermissions.isHistoryReadAvailable())
+        }
+    }
+
+    override fun requestHistoryReadPermission(promise: Promise) {
+        UiThreadUtil.runOnUiThread {
+            val owner = reactApplicationContext.currentActivity as? HealthPermissionOwner
+            if (owner == null) promise.reject("permission_unavailable", "Open HealthEntry to grant permission.")
+            else owner.healthPermissions.requestHistoryRead(promise)
+        }
+    }
+
     companion object { const val NAME = "HealthPermissions" }
 }
