8d959646e82ed9265213724776e4a4c55c64487e
snowolf001 <tyindl@gmail.com>
2026-09-23 18:59:43 -0600

    Add stable native Health Connect read permission request

--- specs/NativeHealthPermissions.ts
+++ specs/NativeHealthPermissions.ts
@@ -2,6 +2,7 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 export interface Spec extends TurboModule {
   requestWritePermission(recordType: string): Promise<boolean>;
+  requestReadPermissions(recordTypes: string[]): Promise<boolean>;
   isHistoryReadAvailable(): Promise<boolean>;
   requestHistoryReadPermission(): Promise<boolean>;
 }
